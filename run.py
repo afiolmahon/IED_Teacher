b3e8d9fbae5b68c6880a4cc2b2b5a9ee4b21c95b
Antonio Fiol-Mahon <afiolmahon@gmail.com>
2017-04-21 17:06:46 -0400

    Progress today

--- run.py
+++ run.py
@@ -9,6 +9,32 @@ import threading
 # FLASK CONFIGURATION
 app = flask.Flask(__name__)
 
+# RADIO CONFIGURATION
+
+NAMES = {
+        32: 'Student 1',
+        33: 'Student 2'
+        } # Device ID Alias for teacher interface
+
+
+w_pipes = { # pipes for writing to a device id
+    32: [0xc2, 0xc2, 0xc2, 0xc2, 0xc2],
+    33: [0xe7, 0xe7, 0xe7, 0xe7, 0xe7]
+}
+r_pipes = { # pipes for reading from a device id
+    32: [0xe7, 0xe7, 0xe7, 0xe7, 0xe7],
+    33: [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]
+}
+
+r = NRF24()
+rf_lock = threading.Lock()  # Regulates access to RF Radio
+CLASS_ID = 0x00
+device_id = 32
+polling = False
+
+devices = [32, 33]
+
+
 @app.route('/')
 def index():
     return flask.render_template('index.html')
@@ -26,26 +52,11 @@ def clear_question():
     global polling
     polling = False  # Stops Poll thread if it's running
     print('Clearing LEDs...')
-    r_write(33)
+    for dev_id in devices:
+        r_write(33, dev_id)
     print('write complete...')
     return flask.redirect(flask.url_for('index'))
 
-
-# RADIO CONFIGURATION
-
-NAMES = {32: 'Student 1'} # Device ID Alias for teacher interface
-w_pipes = { # pipes for writing to a device id
-    32: [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]
-}
-r_pipes = { # pipes for reading from a device id
-    32: [0xe7, 0xe7, 0xe7, 0xe7, 0xe7]
-}
-
-r = NRF24()
-rf_lock = threading.Lock()  # Regulates access to RF Radio
-CLASS_ID = 0x00
-device_id = 32
-polling = False
 
 """
 operations
@@ -64,7 +75,7 @@ def setup_radio(details=True, write_pipe=w_pipes[32], read_pipe=r_pipes[32]):
     #r.setPayloadSize(8)
     r.setChannel(0x60)
     r.setDataRate(NRF24.BR_250KBPS)
-    r.enableDynamicPayloads();
+    r.enableDynamicPayloads()
     r.setPALevel(NRF24.PA_MAX)
     r.setAutoAck(1)
     r.enableAckPayload()
@@ -76,9 +87,10 @@ def setup_radio(details=True, write_pipe=w_pipes[32], read_pipe=r_pipes[32]):
         r.printDetails()
 
 
-def r_write(opcode, operand=0, dev_id=device_id, class_id=0):
+def r_write(opcode, dev_id, operand=0, class_id=0):
     payload = [class_id, dev_id, opcode, operand, 0x00, 0x00, 0x00, 0x00]
     rf_lock.acquire()
+    r.openWritingPipe(w_pipes[dev_id]) # Open correct com pipe
     if r.write(payload):
         print('<<', payload)
         rf_lock.release()
@@ -87,10 +99,11 @@ def r_write(opcode, operand=0, dev_id=device_id, class_id=0):
     return False
 
 
-def read_resp(max_retry=20):
+def read_resp(dev_id, max_retry=20):
     buff = [0, 0, 0, 0, 0, 0, 0, 0]
     retry = 0
     rf_lock.acquire()
+    r.openReadingPipe(1, r_pipes[dev_id])
     r.startListening()
     while not r.available() and retry < max_retry:
         retry += 1
@@ -105,33 +118,35 @@ def read_resp(max_retry=20):
     return None
 
 
-def poll_student(dev_id=32, duration=3):
+def poll_student(duration=6):
     answers = {}
 
     def handle_recv(buff):
         device = buff[1]
         opcode = buff[2]
         if opcode == 17:  # Button Closed Opcode
-            r_write(32)
-            answers[device_id] = True
+            r_write(32, device)
+            answers[device] = True
             print(NAMES[device] + ' Button Pressed')
 
     global polling
     polling = True
     start_t = time.time()
     print('poll_student Question')
-    print('Write Color', r_write(10))  # Set Device to answer Color
+    for dev_id in devices:
+        print('Write Color', r_write(10, dev_id))  # Set Device to answer Color
     while time.time() - start_t < duration and polling:
-        r_write(16)
-        time.sleep(1/100)
-        buff = read_resp()
-        if buff:
-            handle_recv(buff)
+        for dev in devices:
+            r_write(16, dev)
+            time.sleep(1/100)
+            buff = read_resp(dev)
+            if buff:
+                handle_recv(buff)
 
 
-
-    if dev_id not in answers:
-        r_write(33)
+    for dev_id in devices:
+        if dev_id not in answers:
+            r_write(33, dev_id)
     polling = False
     return answers
 
